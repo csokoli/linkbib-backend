cfafbdf1900a5994799a65614f2c30af9a024b0a
Sokoli <Christian.Sokoli@adesso.de>
2018-12-28 10:57:12 +0100

    Initial commit

---
+++ src/main/java/com/csokoli/linkbib/LinkbibliothekApplication.java
@@ -0,0 +1,14 @@
+package com.csokoli.linkbib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkbibliothekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkbibliothekApplication.class, args);
+    }
+
+}
+
